e071cb12b8d11d8806803d425bfe1f5908bbf69a
furkannyildirimm <furkannyildirimm@hotmail.com>
2024-05-21 00:38:51 +0300

    Added UIView+Extension

--- Pollexa/Helpers/Extensions/UIView+Extension.swift
+++ Pollexa/Helpers/Extensions/UIView+Extension.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 extension UIView {
+    
     @IBInspectable var cornerRadius: CGFloat {
         get{ return self.cornerRadius}
         set{
             self.layer.cornerRadius = newValue
         }
     }
+    
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+         let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+         let mask = CAShapeLayer()
+         mask.path = path.cgPath
+         layer.mask = mask
+     }
 }
