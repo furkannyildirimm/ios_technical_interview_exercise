f35f6254b734a526d3ac559bf946da547f361e46
furkannyildirimm <furkannyildirimm@hotmail.com>
2024-05-22 01:07:03 +0300

    Date+TableviewCellRegister Extensions added.

---
+++ Pollexa/Helpers/Extensions/ClassName+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  ClassName+Extension.swift
+//  Pollexa
+//
+//  Created by Furkan Yıldırım on 20.05.2024.
+//
+
+import UIKit
+
+public protocol ClassNameProtocol {
+    static var className: String { get }
+    var className: String { get }
+}
+
+public extension ClassNameProtocol {
+    static var className: String {
+        return String(describing: self)
+    }
+
+    var className: String {
+        return type(of: self).className
+    }
+}
+
+extension NSObject: ClassNameProtocol {}
